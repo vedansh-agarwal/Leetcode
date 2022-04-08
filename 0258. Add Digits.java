bab109ddbadb4927dd555bf4d13d670719ff8552
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-04-08 23:08:22 +0530

    Add files via upload

---
+++ 0258. Add Digits.java
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/add-digits/
+ */
+class Solution {
+    public int addDigits(int num){
+        int sum = 0;
+        while(num > 0) {
+            sum += num%10;
+            num /= 10;
+        }
+        if(sum < 10)
+            return sum;
+        return addDigits(sum);
+    }
+}
