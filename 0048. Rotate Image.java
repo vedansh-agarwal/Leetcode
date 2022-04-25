8735aa29758e0372c4a29fe44ac919f99aeedd12
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-04-25 20:54:58 +0530

    Create 0048. Rotate Image.java

---
+++ 0048. Rotate Image.java
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/rotate-image/
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++) 
+            for(int j = i+1; j < n; j++) 
+                swap(matrix, i, j, j, i);
+        
+        for(int i = 0; i < n/2; i++) 
+            for(int j = 0; j < n; j++) 
+                swap(matrix, j, i, j, n-i-1);
+    }
+    
+    private void swap(int matrix[][], int x1, int y1, int x2, int y2) {
+        int temp = matrix[x1][y1];
+        matrix[x1][y1] = matrix[x2][y2];
+        matrix[x2][y2] = temp;
+    }
+}
